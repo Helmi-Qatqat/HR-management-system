ba68523160be0433b9ad4b04faed4976ddb25e79
Helmi-Qatqat <helmiqatqat@gmail.com>
2023-04-13 14:27:36 +0300

    Static Employees added

--- app.js
+++ app.js
@@ -20,6 +20,31 @@ let calcSalary = function (level) {
 };
 let EmployeesArray = [];
 
+let Renderer = function (id, fullName, department, level, imgURL) {
+  //Element Create
+  let myContainer = document.querySelector(`.${department.toLowerCase()} .emp-container`)
+  let empCard = document.createElement('div')
+  let empImg  = document.createElement('img')
+  let empParagraph = document.createElement('p')
+  let textInfo = `<p>ID: ${id}</p><p>Name: ${fullName}</p><p>Department: ${department}</p><p>Level: ${level}</p><p>Salary: ${calcSalary(level)} $</p>`
+  //Element appending
+  myContainer.appendChild(empCard);
+  empCard.appendChild(empImg);
+  if(imgURL === undefined) {
+    empImg.src = `./assets/${fullName.split(" ")[0]}.jpg`
+  } else {
+    empImg.src = imgURL
+  }
+  empCard.appendChild(empParagraph);
+  empParagraph.innerHTML = textInfo
+  //Element Styling
+  empCard.style.cssText =
+    "display: flex; flex-flow: column; padding: 15px; background-color: #fafafa; border-radius: 5px; width: 300px; align-items: center; gap: 10px; border: 5px solid rgb(5, 120, 155)";
+  empImg.style.cssText =
+    "width: 200px; border-radius: 5px";
+  
+}
+
 function Employee(id, fullName, department, level, img, salary) {
   this["Employee ID"] = id;
   this["Full Name"] = fullName;
@@ -28,53 +53,30 @@ function Employee(id, fullName, department, level, img, salary) {
   this["Image URL"] = img;
   this.salary = calcSalary(this.Level);
   EmployeesArray.push(this);
+  Renderer(this["Employee ID"], this["Full Name"], this.Department, this.Level, this["Image URL"]);
 }
+
+new Employee(1000, "Ghazi Samer", "Administration", "Senior");
+new Employee(1001, "Lana Ali", "Finance", "Senior");
+new Employee(1002, "Tamara Ayoub", "Marketing", "Senior");
+new Employee(1003, "Safi Walid", "Administration", "Mid-Senior");
+new Employee(1004, "Omar Zaid", "Development", "Senior");
+new Employee(1005, "Rana Saleh", "Development", "Junior");
+new Employee(1006, "Hadi Ahmad", "Finance", "Mid-Senior");
+
+
 
 let submitBtn = document.querySelector('[type="button"]');
 submitBtn.addEventListener("click", function (event) {
+  //Element Values Selection
   let myForm = event.target.parentElement;
-  let id =
-    EmployeesArray.length >= 1
-      ? EmployeesArray[EmployeesArray.length - 1]["Employee ID"] + 1
-      : 1000;
+  let id = EmployeesArray[EmployeesArray.length - 1]["Employee ID"] + 1;
   let name = myForm.querySelector('[type="name"]').value;
-  let department = myForm.querySelector('[name="department"]')
-    .selectedOptions[0].innerHTML;
-  let level =
-    myForm.querySelector('[name="level"]').selectedOptions[0].innerHTML;
+  let department = myForm.querySelector('[name="department"]').selectedOptions[0].innerHTML;
+  let level = myForm.querySelector('[name="level"]').selectedOptions[0].innerHTML;
   let imgURL = document.querySelector("[type='text']").value;
-  // Create Elements
-  let myCard = document.createElement("div");
-  myCard.style.cssText =
-    "display: flex; flex-flow: column; padding: 15px; background-color: #fafafa; border-radius: 5px; width: 300px; align-items: center; gap: 20px;";
-  let empImg = document.createElement("img");
-  empImg.src = imgURL;
-  empImg.style.cssText =
-    "width: 200px; border-radius: 5px; border: 1px solid black";
-  let empInfoContainer = document.createElement("div");
-  let empInfo = `Name: ${name} - ID: ${id} Department: ${department} - Level: ${level} - Salary: ${calcSalary(
-    level
-  )}`;
-  console.log(document.querySelector("section.administration .emp-container"));
-  myCard.appendChild(empImg, empInfoContainer);
-  empInfoContainer.textContent = empInfo;
-  empInfoContainer.style.cssText = "color: black; text-align: center";
-  myCard.appendChild(empInfoContainer);
-  switch (department) {
-    case "Administration":
-      document.querySelector("section.administration .emp-container")
-        .appendChild(myCard);
-      break;
-    case "Marketing":
-      document.querySelector("section.marketing .emp-container").appendChild(myCard);
-      break;
-    case "Development":
-      document.querySelector("section.development .emp-container")
-        .appendChild(myCard);
-      break;
-    case "Finance":
-      document.querySelector("section.finance .emp-container").appendChild(myCard);
-      break;
-  }
+  console.log(imgURL)
+  //Element Create
   new Employee(id, name, department, level, imgURL, calcSalary(level));
 });
+
